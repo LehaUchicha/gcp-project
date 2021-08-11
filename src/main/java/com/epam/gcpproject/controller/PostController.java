069b68707d102bb6ea339ad6775e02ccc0504cf7
Aleksei Riasnov <aleksei_riasnov@epam.com>
2021-08-11 23:59:27 +0300

    use secret manager

--- src/main/java/com/epam/gcpproject/controller/PostController.java
+++ src/main/java/com/epam/gcpproject/controller/PostController.java
@@ -7,10 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
