39ae1f16ddd3669d11a2ef1c3cea9f4370985345
Aleksei Riasnov <aleksei_riasnov@epam.com>
2021-08-11 19:47:02 +0300

    added sql migration tool

--- src/main/java/com/epam/gcpproject/model/Post.java
+++ src/main/java/com/epam/gcpproject/model/Post.java
@@ -4,23 +4,30 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "posts")
 public class Post {
 
     @Id
     private String id;
 
+    @Column(name = "title")
     private String title;
 
+    @Column(name = "description")
     private String description;
 
+    @Column(name = "full_text")
     private String fullText;
 
+    @Column(name = "author")
     private String author;
 }
