ca101648c185879589c346146a830287914e3fd4
Aleksei Riasnov <aleksei_riasnov@epam.com>
2021-08-11 18:33:34 +0300

    make Post as entity

--- src/main/java/com/epam/gcpproject/model/Post.java
+++ src/main/java/com/epam/gcpproject/model/Post.java
@@ -4,11 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Post {
 
+    @Id
     private String id;
 
     private String title;
