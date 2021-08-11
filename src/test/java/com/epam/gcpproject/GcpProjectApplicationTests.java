f814139c8de77b22ef300c233be2b4f72ba5c5bf
Aleksei Riasnov <aleksei_riasnov@epam.com>
2021-08-11 18:25:32 +0300

    fix tests

--- src/test/java/com/epam/gcpproject/GcpProjectApplicationTests.java
+++ src/test/java/com/epam/gcpproject/GcpProjectApplicationTests.java
@@ -1,13 +1,23 @@
 package com.epam.gcpproject;
 
+import com.epam.gcpproject.model.Post;
+import com.epam.gcpproject.repository.PostRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
 
 @SpringBootTest
 class GcpProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @MockBean
+    private PostRepository postRepository;
+
+    @Test
+    void contextLoads() {
+        Mockito.when(postRepository.findAll()).thenReturn(List.of(new Post()));
+    }
 
 }
